bebeb39df940e7daa4f9fbc6fe4d39839f51c703
plutozhang99 <lionelzhang99@gmail.com>
2023-11-04 05:24:24 -0700

    Update logger.go

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module tortorCoin
 
 go 1.21
 
-require github.com/sirupsen/logrus v1.9.3
+require (
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/sirupsen/logrus v1.9.3
+)
 
 require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- logger.go
+++ logger.go
@@ -1,8 +1,8 @@
 package tortorCoin
 
-// logger.go
-
+// logger.go 日志模块
 import (
+	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
 	"os"
 )
@@ -10,15 +10,52 @@ import (
 var Log *logrus.Logger
 
 func init() {
+	// 创建日志文件夹
+	if _, err := os.Stat("./log"); os.IsNotExist(err) {
+		err := os.Mkdir("./log", 0755)
+		if err != nil {
+			logrus.Fatalf("Failed to create log directory: %v", err)
+		}
+	}
+
 	// 初始化全局logger实例
 	Log = logrus.New()
 
-	// 设置日志输出格式为JSON
+	// 设置日志格式为JSON
 	Log.SetFormatter(&logrus.JSONFormatter{})
+
+	pathMap := lfshook.PathMap{
+		logrus.InfoLevel:  "./log/info.log",
+		logrus.WarnLevel:  "./log/warn.log",
+		logrus.ErrorLevel: "./log/error.log",
+	}
+	Log.AddHook(lfshook.NewHook(
+		pathMap,
+		&logrus.JSONFormatter{},
+	))
+
+	// 确保日志文件存在
+	ensureLogFileExists("./log/info.log")
+	ensureLogFileExists("./log/warn.log")
+	ensureLogFileExists("./log/error.log")
 
 	// 设置日志级别
 	Log.SetLevel(logrus.InfoLevel)
 
-	// 设置输出目标为标准输出，也可以设置为文件等
+	// 设置输出目标为对应文件
 	Log.SetOutput(os.Stdout)
+}
+
+// 确保日志文件存在
+func ensureLogFileExists(filePath string) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			logrus.Fatalf("Failed to create log file: %s, error: %v", filePath, err)
+		}
+		err = file.Close()
+		if err != nil {
+			logrus.Fatalf("Failed to close log file: %s, error: %v", filePath, err)
+		}
+	}
 }
